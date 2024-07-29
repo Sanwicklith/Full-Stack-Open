3043a5729b4b7a1015a839b020caaea26506a0e7
Your Name <Sanwicklith>
2024-07-29 22:22:48 +0200

    Add filter functionality

--- part_2/Exercise_2.9/src/App.jsx
+++ part_2/Exercise_2.9/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '39-44-5323523' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [person, setPerson] = useState({
     name: '',
-    number: ''
+    number: '',
   });
+  const [filter, setFilter] = useState('');
 
   const handleFormSubmit = e => {
     e.preventDefault();
     const newPersonName = person.name.trim().toLowerCase();
 
     // Extract names from the persons array and convert to lowercase, with a null check
-    const personNames = persons.map(p => p.name ? p.name.toLowerCase() : '');
+    const personNames = persons.map(p => (p.name ? p.name.toLowerCase() : ''));
 
     // Check if newPerson is already in the personNames array
     if (personNames.includes(newPersonName)) {
@@ -28,11 +34,27 @@ const App = () => {
     setPerson({ ...person, [e.target.name]: e.target.value });
   };
 
+  const handleFilter = e => {
+    const filterStr = e.target.value;
+    setFilter(filterStr);
+    const filteredPersons = persons.filter(person => {
+      return person.name.toLowerCase().includes(filterStr.toLowerCase());
+    });
+    setPersons(filteredPersons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div className="filter">
-        filter shown with: <input name='filter' type="text" placeholder="Filter names" />
+        filter shown with:{' '}
+        <input
+          name="filter"
+          type="text"
+          placeholder="Filter names"
+          value={filter}
+          onChange={handleFilter}
+        />
       </div>
       <h2>add a new</h2>
       <form onSubmit={handleFormSubmit}>
@@ -49,7 +71,9 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((p, i) => (
-          <p key={i}>{p.name} {p.number}</p>
+          <p key={i}>
+            {p.name} {p.number}
+          </p>
         ))}
       </div>
     </div>
