2353148583ccf47e2c6ca19d0d532c353b9b32eb
Your Name <Sanwicklith>
2024-07-29 14:54:52 +0200

    add event handlers

--- part_2/Exercise_2.6/src/App.jsx
+++ part_2/Exercise_2.6/src/App.jsx
@@ -6,19 +6,31 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleFormSubmit = (e)=>{
+    e.preventDefault()
+    const newPerson = e.target.name.value
+    console.log(newPerson)
+    setPersons(persons.concat({name:newPerson}))
+    setNewName('')
+  }
+
+  const handleChange = (e)=>setNewName(e.target.value)
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <div>
-          name: <input />
+          name: <input name='name' onChange={handleChange} type='text' value={newName}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button  type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
+      <div>{
+        persons.map((person, i)=>(<p key={i}>{person.name}</p>) )}
+      </div>
     </div>
   )
 }
