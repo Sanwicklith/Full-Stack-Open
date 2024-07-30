6235492b0102ce40bff5dd12f2184cdff6636a2c
Your Name <Sanwicklith>
2024-07-30 17:16:47 +0200

    setup data fetching function and use useEffect to call the function on loading

--- part_2/Exercise_2.11/src/App.jsx
+++ part_2/Exercise_2.11/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      setPersons(res.data); // console.log(persons);
+    });
+  };
+  useEffect(hook, []);
+
   const [person, setPerson] = useState({
     name: '',
     number: '',
@@ -42,16 +46,18 @@ const App = () => {
   };
 
   // Filter persons based on the filter state
-  const filteredPersons = persons.filter(p =>
-    p.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()));
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
-      <PersonForm person={person} handleFormChange={handleFormChange} handleFormSubmit={handleFormSubmit}/>
+      <PersonForm
+        person={person}
+        handleFormChange={handleFormChange}
+        handleFormSubmit={handleFormSubmit}
+      />
       <h2>Numbers</h2>
       <Persons filteredPersons={filteredPersons} />
     </div>
